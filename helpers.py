ca9244b895f4a652861f95ca1fc8e9f81257366f
ijdykeman <ijdykeman@gmail.com>
2016-04-14 00:50:20 -0500

    neural net training, but not well

--- helpers.py
+++ helpers.py
@@ -29,9 +29,14 @@ def zero_to_one(array):
     # array = array - np.min(array)
     # array = 2*(array/np.max(array))-1
     # return np.nan_to_num(array)
-    array = array.replace([np.inf, -np.inf], 1)
-    array = array.fillna(0)
+
+
     scaler = sklearn.preprocessing.MinMaxScaler(feature_range = (-1,1))
+    array[array == np.inf] = 0
+    
+    array[array == -np.inf] = 0
+    array[array == np.nan] = 0
+    array = array.fillna(0)
     return scaler.fit_transform(array)
 
 
@@ -42,26 +47,27 @@ def load_dataset(path, scale=True, include_hcad = True):
     df = pandas.DataFrame(data)
     if scale:
         for label in df._get_numeric_data().columns:
-            if label != 'hcad':
-                df[label] = df[label].astype(float)
-                df[label] = zero_to_one(df[label])
-                df[label][df[label] > 1] = 1.0
+            #if label != 'hcad':
+            df[label] = df[label].astype(float)
+            df[label] = zero_to_one(df[label])
+            df[label][df[label] > 1] = 1.0
     if include_hcad:
         df['hcad'] = df['hcad'].astype(int)
     df = df.replace([np.inf, -np.inf], 1)
     
     return df.sort(['hcad']).fillna(0)
     
-def fast_tsne(df_data, dest_folder, n = None, file_tag= "", embedded_dimensions=2, perplexity = 50):
-    result_2d = {}
-    result_2d['hcad'] = df_data['hcad'][:n]
+def fast_tsne(df_data, dest_folder="", n = None, file_tag= "", embedded_dimensions=2, perplexity = 50):
+
     df_data = df_data.drop('hcad', 1) # don't embed the hcad number!
     df_data = np.array(df_data)[:n]
-    embedding = bh_sne(np.array(df_data)[:n], perplexity=perplexity)
-#     embedding = bh_sne(np.array(df_data))
-    result_2d['x'] = zero_to_one(embedding[:, 0])
-    result_2d['y'] = zero_to_one(embedding[:, 1])
-    result_2d = pandas.DataFrame.from_dict(result_2d)
+    embedding = bh_sne(np.array(df_data)[:n], perplexity=perplexity, d = embedded_dimensions)
+
+    # result_2d = {}
+    # result_2d['hcad'] = df_data['hcad'][:n]
+    # result_2d['x'] = zero_to_one(embedding[:, 0])
+    # result_2d['y'] = zero_to_one(embedding[:, 1])
+    # result_2d = pandas.DataFrame.from_dict(result_2d)
     #name = file_tag+"_"+"_".join(df_data.columns)[:40] + "_n:"+str(len(result))
     #result.to_pickle(dest_folder+name)
     return embedding
@@ -73,7 +79,6 @@ def slow_tsne(df_data, dest_folder, n = None, file_tag= "", embedded_dimensions=
     df_data = df_data.drop('hcad', 1) # don't embed the hcad number!
     df_data = np.array(df_data)[:n]
     embedding = np.array(list(bh_tsne(np.array(df_data)[:n], perplexity=perplexity)))
-#     embedding = bh_sne(np.array(df_data))
     print(embedding)
 
     result_2d['x'] = zero_to_one(embedding[:, 0])
@@ -90,9 +95,7 @@ def hist_2d(vis_x,vis_y):
     plt.imshow(np.flipud(hh.T),cmap='jet', interpolation='none', shape = (1,1))
     plt.colorbar()
     
-def get_where_img0_is_1(pddf):
-    img0_metadata = (META.loc[META['img0'] == 1])
-    return pddf.loc[pddf['hcad'].isin(list(img0_metadata['hcad']))]
+
 
 def pairwise_plot(pddf, sqrt = False):
     if sqrt:
@@ -148,6 +151,20 @@ def shuffle_in_unison(a, b):
     numpy.random.set_state(rng_state)
     numpy.random.shuffle(b)
         
+
+def train_test_split(x_mat_in, y_col_in):
+    x_mat = x_mat_in.copy()
+    y_col = y_col_in.copy()
+
+    shuffle_in_unison(x_mat, y_col)
+    X_train = np.expand_dims(np.expand_dims(x_mat[:4*len(y_col)/300], axis=1), axis=3)
+    y_train = y_col[:4*len(y_col)/300] # limit training data amount, as opposed to 600000
+    X_val = np.expand_dims(np.expand_dims(x_mat[4*len(y_col)/7: 6*len(y_col)/7], axis=1), axis=3)
+    y_val = y_col[4*len(y_col)/7: 6*len(y_col)/7]
+    X_test = np.expand_dims(np.expand_dims(x_mat[6*len(y_col)/7:], axis=1), axis=3)
+    y_test = y_col[6*len(y_col)/7:]
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
 def load_mega_hcad():
     # hcad = load_dataset("/home/isaac/Dropbox/data_for_brian/hcad_features/hcad_df.hd")
 #     hcad_data = img0_hcad_data
@@ -177,7 +194,6 @@ def load_mega_hcad():
     y_data_np = np.concatenate((y_data_np_nonzero, y_data_np_zero), axis=0)
     #mega_hcad.append(mega_hcad_zero)
     #y_data
-    shuffle_in_unison(mega_hcad, y_data_np)
 
     
     print("hcad length", (mega_hcad.shape))
@@ -185,18 +201,23 @@ def load_mega_hcad():
     #print (mega_hcad)
 
 
-    X_train = np.expand_dims(np.expand_dims(mega_hcad[:4*len(y_data_np)/300], axis=1), axis=3)
-    y_train = 1-y_data_np[:4*len(y_data_np)/300, y_column] # limit training data amount, as opposed to 600000
+    return train_test_split(mega_hcad, y_data_np[:, y_column])
 #     print("y train",y_train.shape)
-    X_val = np.expand_dims(np.expand_dims(mega_hcad[4*len(y_data_np)/7: 6*len(y_data_np)/7], axis=1), axis=3)
-    y_val = 1-y_data_np[4*len(y_data_np)/7: 6*len(y_data_np)/7, y_column]
-    X_test = np.expand_dims(np.expand_dims(mega_hcad[6*len(y_data_np)/7:], axis=1), axis=3)
-    y_test = 1-y_data_np[6*len(y_data_np)/7:, y_column]
+
+    # shuffle_in_unison(mega_hcad, y_data_np)
+    # X_train = np.expand_dims(np.expand_dims(mega_hcad[:4*len(y_data_np)/300], axis=1), axis=3)
+    # y_train = y_data_np[:4*len(y_data_np)/300, y_column] # limit training data amount, as opposed to 600000
+    # X_val = np.expand_dims(np.expand_dims(mega_hcad[4*len(y_data_np)/7: 6*len(y_data_np)/7], axis=1), axis=3)
+    # y_val = y_data_np[4*len(y_data_np)/7: 6*len(y_data_np)/7, y_column]
+    # X_test = np.expand_dims(np.expand_dims(mega_hcad[6*len(y_data_np)/7:], axis=1), axis=3)
+    # y_test = y_data_np[6*len(y_data_np)/7:, y_column]
+    # return X_train, y_train, X_val, y_val, X_test, y_test
 #     plt.hist(y_train)
 #     plt.show()
 #     plt.hist(y_val)
 #     plt.show()
 #     plt.hist(y_test)
 #     plt.show()
-    return X_train, y_train, X_val, y_val, X_test, y_test
 # load_mega_hcad()
+
+print ("helpers executed")
