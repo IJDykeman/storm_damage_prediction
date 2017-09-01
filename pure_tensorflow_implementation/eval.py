13253b40f1893d5e301a6c114a413a542417dc32
Isaac Dykeman <ijdykeman@gmail.com>
2017-09-01 01:51:13 -0500

    slight cleanup

--- pure_tensorflow_implementation/eval.py
+++ pure_tensorflow_implementation/eval.py
@@ -1,9 +1,11 @@
+# this is to make the environment compatible with the Atom Hydrogen
+# interactive editor package.  You can ignore this code if that doesn't mean
+# anything to you.
 PYSOLR_PATH = '/home/isaac/Desktop/storm_damage_prediction/pure_tensorflow_implementation'
-# %matplotlib inline
-
 import sys
 if not PYSOLR_PATH in sys.path:
     sys.path.append(PYSOLR_PATH)
+#########
 
 import model
 reload(model)
@@ -16,22 +18,9 @@ import sklearn.manifold
 print sklearn.metrics.roc_auc_score
 
 data_handler = data_loading.DataHandler()
-tf.reset_default_graph()
+# load the model and restore the weights from the latest checkpoint
 model = model.Model()
-
-
 model.restore()
-# batch_indices = test_indices[i * batch_size:(i+1) * batch_size]
-#
-# metamat, extra_features, y_true = data_handler.get_data_batch_from_indices(batch_indices)
-# ground_truth.extend(y_true[:, 1])
-# plt.hist( np.array(model.sess.run(model.heightmap_image_batch,
-#     feed_dict={model.heightmap_ph:metamat,
-#     model.extra_features_ph:extra_features,
-#     model.labels_ph:y_true,
-#     model.keep_prob_ph: 1})).flatten())
-# plt.show()
-
 
 test_indices = data_handler.test_indices[:]
 import random
@@ -39,7 +28,7 @@ random.shuffle(test_indices)
 batch_size = 32
 predictions = []
 ground_truth = []
-for i in range(10):
+for i in range(50):
     print i
     batch_indices = test_indices[i * batch_size:(i+1) * batch_size]
 
@@ -52,10 +41,4 @@ for i in range(10):
         model.keep_prob_ph: 1})
     predictions.extend(pred_probabilities[:, 1])
 
-print np.array(ground_truth)
-print np.array(predictions)
 print "auc:", sklearn.metrics.roc_auc_score(ground_truth, np.array(predictions), average='macro', sample_weight=None)
-
-# plt.hist(ground_truth)
-plt.hist(predictions)
-plt.show()
